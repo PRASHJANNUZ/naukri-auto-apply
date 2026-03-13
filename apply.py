c998a44897bd33e674f131674a3c7dc8748b9023
Prashant <143874787+PRASHJANNUZ@users.noreply.github.com>
2026-03-13 15:55:10 +0530

    Create apply.py

---
+++ apply.py
@@ -0,0 +1,58 @@
+from playwright.sync_api import sync_playwright
+import os
+import time
+
+EMAIL = os.getenv("prashantjannu1313@gmail.com")
+PASSWORD = os.getenv("Prashant302")
+
+with sync_playwright() as p:
+
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
+
+    page.goto("https://www.naukri.com/nlogin/login")
+
+    page.fill("#usernameField", EMAIL)
+    page.fill("#passwordField", PASSWORD)
+
+    page.click("button[type='submit']")
+    page.wait_for_timeout(8000)
+
+    # Go to profile
+    page.goto("https://www.naukri.com/mnjuser/profile")
+
+    page.wait_for_timeout(5000)
+
+    print("Profile refreshed")
+
+    # Search Java jobs
+    page.goto("https://www.naukri.com/java-developer-jobs")
+
+    page.wait_for_timeout(5000)
+
+    jobs = page.locator("a.title").all()
+
+    count = 0
+
+    for job in jobs:
+
+        if count >= 20:
+            break
+
+        try:
+            job.click()
+            page.wait_for_timeout(4000)
+
+            apply_btn = page.locator("button:has-text('Apply')")
+            apply_btn.click()
+
+            print("Applied to job")
+
+            count += 1
+
+            page.wait_for_timeout(4000)
+
+        except:
+            pass
+
+    browser.close()
